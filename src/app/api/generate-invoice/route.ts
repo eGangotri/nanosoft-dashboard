7b971017df1489cf8356f25ffd60d54612582828
writetochetanpandey <writetochetanpandey@gmail.com>
2024-10-17 19:38:54 +0800

    -

--- src/app/api/generate-invoice/route.ts
+++ src/app/api/generate-invoice/route.ts
@@ -11,34 +11,33 @@ export async function POST(req: NextRequest) {
     const templatePath = path.join(process.cwd(), 'public', `${company}-${template}.xlsx`)
     console.log(`templatePath: ${templatePath}`);
 
-    try {
-      if (!fs.existsSync(templatePath)) {
-        console.log(`Cannot access: ${templatePath}`);
-        return NextResponse.json({ error: `${templatePath} No Template configured for Company(${company}) - Client(${template}).` }, { status: 300 })
-      }
-
-      const workbook = new ExcelJS.Workbook()
-      await workbook.xlsx.readFile(templatePath)
-
-      // Get the first worksheet
-      const worksheet = workbook.worksheets[0]
-
-      // Update cell C11 with the invoice number
-      const cell = worksheet.getCell('F7')
-      cell.value = invoiceNumber
-
-      // Write the updated workbook to a buffer
-      const buffer = await workbook.xlsx.writeBuffer()
-
-      // Set the appropriate headers for file download
-      const headers = new Headers()
-      headers.append('Content-Disposition', `attachment; filename="Invoice-${invoiceNumber}-${company}-${template}.xlsx"`)
-      headers.append('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-      // Return the file as a downloadable response
-      return new NextResponse(buffer, { status: 200, headers })
-    } catch (error) {
-      console.error('Error generating invoice:', error)
-      return NextResponse.json({ error: 'Failed to generate invoice' }, { status: 500 })
+    if (!fs.existsSync(templatePath)) {
+      console.log(`Cannot access: ${templatePath}`);
+      return NextResponse.json({ error: `${templatePath} No Template configured for Company(${company}) - Client(${template}).` }, { status: 300 })
     }
+
+    const workbook = new ExcelJS.Workbook()
+    await workbook.xlsx.readFile(templatePath)
+
+    // Get the first worksheet
+    const worksheet = workbook.worksheets[0]
+
+    // Update cell C11 with the invoice number
+    const cell = worksheet.getCell('F7')
+    cell.value = invoiceNumber
+
+    // Write the updated workbook to a buffer
+    const buffer = await workbook.xlsx.writeBuffer()
+
+    // Set the appropriate headers for file download
+    const headers = new Headers()
+    headers.append('Content-Disposition', `attachment; filename="Invoice-${invoiceNumber}-${company}-${template}.xlsx"`)
+    headers.append('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+    // Return the file as a downloadable response
+    return new NextResponse(buffer, { status: 200, headers });
+  } catch (error) {
+    console.error('Error generating invoice:', error);
+    return NextResponse.json({ error: 'Failed to generate invoice' }, { status: 500 });
   }
+}
